d52bb584b6449b29512b9e77be0e1faa8fcbc76e
Anshul <129313433+anshullk@users.noreply.github.com>
2025-11-19 12:00:08 +0530

    port updated

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def predict():
     return jsonify({"results": results}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
